0da726c07073fef55b392821541349b47e09bf89
Debrabelge <Thomas-db@hotmail.fr>
2020-02-03 23:55:26 +0100

    Footer - added square + text

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,8 +1,16 @@
 <template>
     <footer class="Footer">
+        <div class="Footer__rotated">
+
+        </div>
         <div class="Footer__client-infos">
             <div class="client-infos__content content p-32">
-                OK FOOTER
+                <p>{{$languages.strings.not_hesitate_to_contact_me[$languages.country]}}</p>
+                <br />
+                <p class="font-semibold">France Ehny Sophrologue Praticienne</p>
+                <p>35 cours Evrard de Fayolle 33000 Bordeaux</p>
+                <p>Email: france.ehny@gmail.com</p>
+                <p>Téléphone: 06 08 77 88 95</p>
             </div>
         </div>
         <div class="Footer__rights">
