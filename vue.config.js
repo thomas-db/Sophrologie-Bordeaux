5e88830dc2d0f2be4d777861c990a4250fcfcaf3
Debrabelge <Thomas-db@hotmail.fr>
2022-12-21 16:48:54 +0100

    vue config removed

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    lintOnSave: false,
-    publicPath: '/',
-    configureWebpack: {
-        output: {
-            filename: 'js/[name].[hash].js',
-            chunkFilename: 'js/[id].[hash].js'
-        },
-        devtool: false,
-    },
-}
+// module.exports = {
+//     lintOnSave: false,
+//     publicPath: '/',
+//     configureWebpack: {
+//         output: {
+//             filename: 'js/[name].[hash].js',
+//             chunkFilename: 'js/[id].[hash].js'
+//         },
+//         devtool: false,
+//     },
+// }
